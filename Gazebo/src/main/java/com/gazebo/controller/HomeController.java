0dd1a2d47ddfa90e8a1c04c9970abf2e7117e5f6
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2018-03-12 13:50:44 +0200

    Converted project to use Spring Boot.

---
+++ Gazebo/src/main/java/com/gazebo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.gazebo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Test";
+    }
+}
