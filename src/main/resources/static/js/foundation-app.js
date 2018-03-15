d799a3421374c20a96b1736e5a4af9454f3c6cd6
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2018-03-15 12:21:11 +0200

    Pressing "Register" button closes the login modal and opens the register modal.

--- src/main/resources/static/js/foundation-app.js
+++ src/main/resources/static/js/foundation-app.js
@@ -1,1 +1,9 @@
-$(document).foundation()
+(function () {
+    $(document).foundation();
+
+    $('#login-form-register').on('click', function(e) {
+       e.preventDefault();
+       $('#login-modal').foundation('close');
+       $('#register-modal').foundation('open');
+    });
+})();
