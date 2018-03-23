0d9bc9ebd3a602569a8c294acb9211052638c489
Gediminas <gedgeds@gmail.com>
2018-03-23 12:48:02 +0200

    Created class to validate if password has:     at least 8 characters     at least one lowercase     at least one uppercase     at least one non-letter symbol.

--- src/main/java/lt/vu/mif/Utils/ValidationUtils.java
+++ src/main/java/lt/vu/mif/Utils/ValidationUtils.java
@@ -1,5 +1,16 @@
 package lt.vu.mif.Utils;
 
+import java.util.regex.Pattern;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class ValidationUtils {
+
+    private final String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d|(?=.*[$@!%*?&]))[a-zA-Z\\d$@!%*?&]{8,}$";
+
+    public static boolean ValidatePassword(String password){
+
+        return Pattern.matches(regex, password);
+    }
 
 }
