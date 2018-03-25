4f5cd9e2df0fdd6b8e65708e2fb35a7aa2527ece
Monika Gasparaite <monika9623@gmail.com>
2018-03-25 14:34:07 +0300

    GZ-0006: users and products added to DB

--- src/main/java/lt/vu/mif/App.java
+++ src/main/java/lt/vu/mif/App.java
@@ -1,16 +1,22 @@
 package lt.vu.mif;
 
+import lt.vu.mif.Test.DataGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import lt.vu.mif.Configuration.ServletConfiguration;
+import org.springframework.context.annotation.Import;
 
 @Import({ServletConfiguration.class})
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(App.class);
+        DataGenerator dataGenerator = context.getBean(DataGenerator.class);
+
+        dataGenerator.insertProducts();
+        dataGenerator.insertUsers();
     }
 }
