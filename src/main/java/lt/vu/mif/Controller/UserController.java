2503dab98b59c42f8906418206837d76f0c7001c
gedgeds <gedgeds@gmail.com>
2018-03-25 13:55:06 +0300

    Fixed error caused by Role type parameter in UserController

--- src/main/java/lt/vu/mif/Controller/UserController.java
+++ src/main/java/lt/vu/mif/Controller/UserController.java
@@ -2,6 +2,8 @@ package lt.vu.mif.Controller;
 
 import javax.inject.Named;
 
+//import lt.vu.mif.Entity.Roles.Role;
+import lt.vu.mif.Entity.Roles.Role;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +16,7 @@ import lt.vu.mif.View.UserView;
 @Getter
 @Controller
 public class UserController {
-    private UserView userView = new UserView(10251L, "test@gmail.com");
+    private UserView userView = new UserView(10251L, "test@gmail.com", Role.USER);
 
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public ModelAndView getUserWindow() {
