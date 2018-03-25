33f83c59221e1946fa3b15ad524d3025e254638f
Monika Gasparaite <monika9623@gmail.com>
2018-03-25 14:37:54 +0300

    JSF configuration class removed

--- src/main/java/lt/vu/mif/App.java
+++ src/main/java/lt/vu/mif/App.java
@@ -5,10 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import lt.vu.mif.Configuration.ServletConfiguration;
-import org.springframework.context.annotation.Import;
-
-@Import({ServletConfiguration.class})
 @SpringBootApplication
 public class App {
 
