5fa9e190d3b3aa9c74b193fc6685e2bfb3064fa8
MonikaGasparaite <monika.gasparaite@gmail.com>
2018-03-28 23:22:31 +0300

    GZ-0015: product search by title

--- src/main/java/lt/vu/mif/Repository/ProductRepository.java
+++ src/main/java/lt/vu/mif/Repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package lt.vu.mif.Repository;
 
 import lt.vu.mif.Entity.Product;
+import lt.vu.mif.Entity.Product_;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.criteria.CriteriaBuilder;
@@ -8,6 +9,7 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 
 @Transactional
 @Repository
@@ -18,6 +20,19 @@ public class ProductRepository extends BaseRepository<Product> {
         CriteriaQuery<Product> criteria = builder.createQuery(Product.class);
         Root<Product> root = criteria.from(Product.class);
 
+        criteria.select(root);
+
+        return getEntityManager().createQuery(criteria).getResultList();
+    }
+
+    public List<Product> getByTitle(String title) {
+        Objects.requireNonNull(title);
+
+        CriteriaBuilder builder = getCriteriaBuilder();
+        CriteriaQuery<Product> criteria = builder.createQuery(Product.class);
+        Root<Product> root = criteria.from(Product.class);
+
+        criteria.where(builder.like(root.get(Product_.title), "%" + title + "%"));
         criteria.select(root);
 
         return getEntityManager().createQuery(criteria).getResultList();
