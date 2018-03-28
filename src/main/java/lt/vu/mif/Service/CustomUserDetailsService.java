664150c31fb06cf1933449c754f0c0344e73f8d5
Emilijus <emilijus@gmail.com>
2018-03-28 17:00:41 +0300

    Added empty security configuration

---
+++ src/main/java/lt/vu/mif/Service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package lt.vu.mif.Service;
+
+import lt.vu.mif.Repository.UserRepository;
+import lt.vu.mif.Entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component(value = "userDetailsService")
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return toUserDetails(userRepository.getUserByEmail(s));
+    }
+
+    private UserDetails toUserDetails(User user) {
+        return org.springframework.security.core.userdetails.User.withUsername(user.getEmail())
+            .password(user.getPassword())
+            .roles(user.getRole().toString()).build();
+    }
+}
