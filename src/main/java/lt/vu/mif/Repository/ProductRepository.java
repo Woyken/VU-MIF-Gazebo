59b69eca0c1a83a3a814b2cfc3e220bb916d12cb
MonikaGasparaite <monika.gasparaite@gmail.com>
2018-03-28 23:32:06 +0300

    GZ-0017: fetch products by id

--- src/main/java/lt/vu/mif/Repository/ProductRepository.java
+++ src/main/java/lt/vu/mif/Repository/ProductRepository.java
@@ -3,11 +3,13 @@ package lt.vu.mif.Repository;
 import lt.vu.mif.Entity.Product;
 import lt.vu.mif.Entity.Product_;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.CollectionUtils;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
+import java.util.Collections;
 import java.util.List;
 
 @Transactional
@@ -30,6 +32,21 @@ public class ProductRepository extends BaseRepository<Product> {
         Root<Product> root = criteria.from(Product.class);
 
         criteria.select(root.get(Product_.id));
+
+        return getEntityManager().createQuery(criteria).getResultList();
+    }
+
+    public List<Product> get(List<Long> ids) {
+        if (CollectionUtils.isEmpty(ids)) {
+            return Collections.emptyList();
+        }
+
+        CriteriaBuilder builder = getCriteriaBuilder();
+        CriteriaQuery<Product> criteria = builder.createQuery(Product.class);
+        Root<Product> root = criteria.from(Product.class);
+
+        criteria.where(root.get(Product_.id).in(ids));
+        criteria.select(root);
 
         return getEntityManager().createQuery(criteria).getResultList();
     }
