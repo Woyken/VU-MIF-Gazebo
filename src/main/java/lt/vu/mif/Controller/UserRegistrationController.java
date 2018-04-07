2c972f887aea2b69d78ca8d37efba6adf63921c1
Emilijus <emilijus@gmail.com>
2018-04-08 00:33:30 +0300

    Removed redundant validation

--- src/main/java/lt/vu/mif/Controller/UserRegistrationController.java
+++ src/main/java/lt/vu/mif/Controller/UserRegistrationController.java
@@ -1,23 +1,19 @@
 package lt.vu.mif.Controller;
 
-import java.util.Objects;
-
 import javax.inject.Named;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import lombok.Getter;
 import lombok.Setter;
 import lt.vu.mif.Entity.Roles;
 import lt.vu.mif.Entity.User;
 import lt.vu.mif.Repository.UserRepository;
-import lt.vu.mif.Utils.ValidationUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Named
 @Getter
 @Setter
 public class UserRegistrationController {
+
     @Autowired
     private UserRepository userRepository;
 
@@ -28,14 +24,8 @@ public class UserRegistrationController {
     private String message;
 
     public void saveUser() {
-        boolean passwordValid = ValidationUtils.isPasswordValid(password);
-        boolean passwordsMatch = Objects.equals(password, passwordRepeat);
-        boolean emailValid = ValidationUtils.isEmailValid(email);
-        boolean userAlreadyExists = userRepository.checkIfUserExists(email);
-
-        if (passwordValid && passwordsMatch && emailValid && !userAlreadyExists) {
-            userRepository.save(new User(new BCryptPasswordEncoder().encode(password), email, Roles.Role.USER));
-            message = "Naudotojas " + email + " sėkmingai sukurtas";
-        }
+        userRepository
+            .save(new User(new BCryptPasswordEncoder().encode(password), email, Roles.Role.USER));
+        message = "Naudotojas " + email + " sėkmingai sukurtas";
     }
 }
