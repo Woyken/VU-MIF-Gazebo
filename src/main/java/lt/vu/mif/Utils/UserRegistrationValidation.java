3f4818c2e952deec9de1f7d26530b0f908f1e97e
Emilijus <emilijus@gmail.com>
2018-04-08 23:33:15 +0300

    Cleanup

--- src/main/java/lt/vu/mif/Utils/UserRegistrationValidation.java
+++ src/main/java/lt/vu/mif/Utils/UserRegistrationValidation.java
@@ -4,26 +4,31 @@ import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
-import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
+import javax.inject.Named;
 import lt.vu.mif.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
-@FacesValidator(value = "UserRegistrationValidation")
+//Injection doesn't work with FacesValidator annotation
+//@FacesValidator(value = "UserRegistrationValidation")
+@Named
+@Component
 public class UserRegistrationValidation implements Validator {
 
     @Autowired
     private UserRepository userRepository;
 
     @Override
-    public void validate(FacesContext facesContext, UIComponent uiComponent, Object o) throws ValidatorException {
+    public void validate(FacesContext facesContext, UIComponent uiComponent, Object o)
+        throws ValidatorException {
 
         String email = (String) ((UIInput) uiComponent.getAttributes().get("email")).getValue();
-        String password = (String) ((UIInput) uiComponent.getAttributes().get("password")).getValue();
-        String passwordRepeat = (String) ((UIInput) uiComponent.getAttributes().get("passwordRepeat")).getValue();
+        String password = (String) ((UIInput) uiComponent.getAttributes().get("password"))
+            .getValue();
+        String passwordRepeat = (String) ((UIInput) uiComponent.getAttributes()
+            .get("passwordRepeat")).getValue();
 
         if (email.isEmpty() || password.isEmpty() || passwordRepeat.isEmpty()) {
             throw new ValidatorException(new FacesMessage("Užpildyti ne visi privalomi laukai"));
@@ -41,8 +46,9 @@ public class UserRegistrationValidation implements Validator {
             throw new ValidatorException(new FacesMessage("Įvesti slaptažodžiai nesutampa"));
         }
 
-        if(userRepository.checkIfUserExists(email)) {
-            throw new ValidatorException(new FacesMessage("Naudotojas su nurodytų el. pašto adresu jau egzistuoja"));
+        if (userRepository.checkIfUserExists(email)) {
+            throw new ValidatorException(
+                new FacesMessage("Naudotojas su nurodytu el. pašto adresu jau egzistuoja"));
         }
     }
 }
