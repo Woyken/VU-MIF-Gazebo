3c82350428a520150d19160a9be634958f803d55
monika.gasparaite <monika.gasparaite@gmail.com>
2018-04-20 01:09:29 +0300

    paging added

--- src/main/java/lt/vu/mif/Controller/ProductController.java
+++ src/main/java/lt/vu/mif/Controller/ProductController.java
@@ -1,8 +1,6 @@
 package lt.vu.mif.Controller;
 
 import java.io.Serializable;
-import java.util.List;
-import java.util.stream.Collectors;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -10,10 +8,13 @@ import lombok.Getter;
 import lombok.Setter;
 import lt.vu.mif.Entity.User;
 import lt.vu.mif.Repository.ProductRepository;
+import lt.vu.mif.Search.ProductSearch;
 import lt.vu.mif.Service.UserService;
+import lt.vu.mif.Utils.Paging;
 import lt.vu.mif.View.ProductView;
 import lt.vu.mif.View.UserView;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 
 @Named
 @Getter
@@ -27,11 +28,13 @@ public class ProductController implements Serializable {
     private UserService userService;
 
     private UserView loggedUser;
-    private List<ProductView> products;
-    private String searchPhrase;
+
+    private ProductSearch productSearch = new ProductSearch();
+
+    private Page<ProductView> productsPage;
+    private Paging paging;
 
     public void onPageLoad() {
-        
         if (FacesContext.getCurrentInstance().getPartialViewContext().isAjaxRequest()) {
             return;
         }
@@ -39,12 +42,40 @@ public class ProductController implements Serializable {
         User user = userService.getLoggedUser();
         loggedUser = user == null ? null : new UserView(user);
 
-        products = productRepository.getAll().stream().map(ProductView::new)
-                .collect(Collectors.toList());
+        resetProductSearch();
+        searchProducts();
+        paging = new Paging(productsPage.getTotalPages());
     }
 
     public void searchProducts() {
-        products = productRepository.getAll().stream().filter(p -> p.getTitle().contains(searchPhrase))
-            .map(ProductView::new).collect(Collectors.toList());
+        productsPage = productRepository.getProductsPage(productSearch).map(ProductView::new);
+    }
+
+    private void resetProductSearch() {
+        productSearch.setMaxPrice(null);
+        productSearch.setMinPrice(null);
+        productSearch.setTitle(null);
+        paging.resetActivePage();
+        paging.setTotalPages(productsPage.getTotalPages());
+    }
+
+    public void next() {
+        paging.next();
+        searchProducts();
+    }
+
+    public void previous() {
+        paging.previous();
+        searchProducts();
+    }
+
+    public void searchProducts(int activePage) {
+        productSearch.setActivePage(activePage);
+        paging.setActivePage(activePage);
+        searchProducts();
+    }
+
+    public int getPagingIndex() {
+        return paging.getIndex();
     }
 }
--- src/main/java/lt/vu/mif/Search/ProductSearch.java
+++ src/main/java/lt/vu/mif/Search/ProductSearch.java
@@ -4,10 +4,19 @@ import java.math.BigDecimal;
 
 import lombok.Getter;
 import lombok.Setter;
+import lt.vu.mif.Constants;
 
 @Getter
 @Setter
 public class ProductSearch {
+    private String title;
     private BigDecimal minPrice;
     private BigDecimal maxPrice;
+
+    private int pageSize = Constants.PAGE_SIZE;
+    private int activePage = Constants.ACTIVE_PAGE;
+
+    public int getActivePage() {
+        return activePage;
+    }
 }
