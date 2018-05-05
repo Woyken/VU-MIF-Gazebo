5b42a108fca0d58a4dc08ca569aa137231207025
gedgeds <gedgeds@gmail.com>
2018-05-05 21:26:16 +0300

    Added discount BE validation so that admin could make item discount 1 to 100 percent.

--- src/main/java/lt/vu/mif/Utils/ValidationUtils.java
+++ src/main/java/lt/vu/mif/Utils/ValidationUtils.java
@@ -12,6 +12,7 @@ public class ValidationUtils {
     private final String YEAR_REGEX = "^\\d{2}$";
     private final String MONTH_REGEX = "^(0?[1-9]|1[012])$";
     private final String CVS_REGEX = "^\\d{3}$";
+    private final String DISCOUNT_REGEX = "^(100|[1-9][0-9]?)$";
 
 
     public static boolean isPasswordValid(String password) {
@@ -37,4 +38,6 @@ public class ValidationUtils {
     public static boolean isCvsValid(String cvs) {
         return Pattern.matches(CVS_REGEX, cvs);
     }
+
+    public static boolean isDiscountValid(String discount) { return Pattern.matches(DISCOUNT_REGEX, discount); }
 }
