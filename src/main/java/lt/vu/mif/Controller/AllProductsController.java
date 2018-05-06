fbab99dc0657d8d4181f75c41e3d7410f2d8741c
Emilijus <emilijus@gmail.com>
2018-05-06 14:19:57 +0300

    all-product-page is now working (real data, item edit and deletion)

---
+++ src/main/java/lt/vu/mif/Controller/AllProductsController.java
@@ -0,0 +1,52 @@
+package lt.vu.mif.Controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
+import lt.vu.mif.Repository.ProductRepository;
+import lt.vu.mif.View.ProductView;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Named
+@ViewScoped
+public class AllProductsController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Getter
+    private List<ProductView> products;
+
+    @Setter
+    Long productToDeleteId;
+
+
+    public void onPageLoad() {
+        if (FacesContext.getCurrentInstance().getPartialViewContext().isAjaxRequest()) {
+            return;
+        }
+
+        updateProducts();
+    }
+
+    private void updateProducts() {
+        products = productRepository.getAll().stream().map(ProductView::new)
+            .collect(Collectors.toList());
+    }
+
+    public String deleteProduct() {
+        if (productToDeleteId == null) {
+            throw new IllegalStateException("productToDelete was not set");
+        }
+
+        productRepository.deleteById(productToDeleteId);
+
+        updateProducts();
+
+        return "admin-all-products?faces-redirect=true";
+    }
+}
