16814fda280a4bd074e29f077dc5602b62d0d7cd
gedgeds <gedgeds@gmail.com>
2018-05-06 12:16:38 +0300

    Made a template for future order list and order preview implementation. Added BE code for retrieving all orders Restored previously deleted OrderView attributes Data generator part for order generation is currently not working

--- src/main/java/lt/vu/mif/Repository/OrderRepository.java
+++ src/main/java/lt/vu/mif/Repository/OrderRepository.java
@@ -3,9 +3,13 @@ package lt.vu.mif.Repository;
 import java.util.List;
 import java.util.Objects;
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import lt.vu.mif.Entity.Order;
 import lt.vu.mif.Entity.Order_;
+import lt.vu.mif.Entity.User;
 import lt.vu.mif.Entity.User_;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
@@ -31,5 +35,10 @@ public class OrderRepository extends SimpleJpaRepository<Order, Long> {
             (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder
                 .equal(root.get(Order_.user).get(User_.id), userId));
         return findAll(specification);
+    }
+
+    public List<Order> getAll() {
+
+        return findAll();
     }
 }
