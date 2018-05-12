2b746cfdfb7700a19eabf20f11d140de3b8920de
monika.gasparaite <monika.gasparaite@gmail.com>
2018-05-12 20:39:02 +0300

    GZ-0411: Order repository changed

--- src/main/java/lt/vu/mif/repository/repository/interfaces/IOrderRepository.java
+++ src/main/java/lt/vu/mif/repository/repository/interfaces/IOrderRepository.java
@@ -12,4 +12,12 @@ public interface IOrderRepository {
     List<Order> findAll();
 
     <S extends Order> List<S> saveAll(Iterable<S> entities);
+
+    List<Order> getCurrentUserOrdersToRate(String currentUserEmail);
+
+    Order get(Long id);
+
+    List<Order> getOrders(List<Long> ids);
+
+    void updateAll(List<Order> orders);
 }
