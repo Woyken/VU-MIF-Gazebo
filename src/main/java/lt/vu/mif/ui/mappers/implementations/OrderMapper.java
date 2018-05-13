60c0f7880e20d70fc94b9d36256fdb92545f086a
gedgeds <gedgeds@gmail.com>
2018-05-13 16:54:48 +0300

    Finally works

--- src/main/java/lt/vu/mif/ui/mappers/implementations/OrderMapper.java
+++ src/main/java/lt/vu/mif/ui/mappers/implementations/OrderMapper.java
@@ -3,6 +3,7 @@ package lt.vu.mif.ui.mappers.implementations;
 import java.util.List;
 import java.util.stream.Collectors;
 import lt.vu.mif.model.order.Order;
+import lt.vu.mif.model.order.OrderProduct;
 import lt.vu.mif.ui.mappers.interfaces.IMapper;
 import lt.vu.mif.ui.view.OrderView;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,8 @@ public class OrderMapper implements IMapper<Order, OrderView> {
 
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private ProductMapper productMapper;
 
     @Override
     public Order toEntity(OrderView view) {
@@ -33,6 +36,7 @@ public class OrderMapper implements IMapper<Order, OrderView> {
         view.setRating(entity.getRating());
         view.setStatus(entity.getStatus());
         view.setUser(userMapper.toView(entity.getUser()));
+        view.setProducts(productMapper.toViews(entity.getProducts().stream().map(OrderProduct::getProduct).collect(Collectors.toList())));
 
         return view;
     }
