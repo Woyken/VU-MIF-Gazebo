1db9ae3a6c80708c0b2c82053f6631c1485c28dc
Emilijus <emilijus@gmail.com>
2018-05-19 15:11:13 +0300

    Added validation for 'add discount' page

--- src/main/java/lt/vu/mif/ui/controller/ProductDiscountController.java
+++ src/main/java/lt/vu/mif/ui/controller/ProductDiscountController.java
@@ -1,15 +1,15 @@
 package lt.vu.mif.ui.controller;
 
 
+import java.math.BigDecimal;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
 import lombok.Getter;
 import lombok.Setter;
 import lt.vu.mif.ui.helpers.interfaces.IProductHelper;
 import lt.vu.mif.ui.view.ProductView;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.faces.context.FacesContext;
-import javax.faces.view.ViewScoped;
-import javax.inject.Named;
 
 @Named
 @Getter
@@ -22,25 +22,31 @@ public class ProductDiscountController {
     private ProductView productView;
 
     private Boolean isProductFound;
-    private String discountAsPrice; // Because it can be a 59.99 (with a '.')
-    private Integer discountAsPercent;
-
+    private BigDecimal discountAsPrice;
+    //BigDecimal type for validation purposes
+    private BigDecimal discountAsPercent;
+    private String startDate;
+    private String startTime;
+    private String endDate;
+    private String endTime;
 
     public void onPageLoad() {
-        if (FacesContext.getCurrentInstance().getPartialViewContext().isAjaxRequest()) { return; }
+        if (FacesContext.getCurrentInstance().getPartialViewContext().isAjaxRequest()) {
+            return;
+        }
 
-        // If navigation query doesn't have a valid product ID - it means we want to
-        // create a discount either for a category or for all of the products
         try {
             productView = productHelper.getProductViewFromNavigationQuery();
             isProductFound = true;
-        } catch (Exception x) {
+        } catch (IllegalArgumentException x) {
+            // If navigation query doesn't have a valid product ID - it means we want to
+            // create a discount either for a category or for all of the products
             productView = new ProductView();
             isProductFound = false;
         }
     }
 
     public void addDiscount() {
-        int a = 5;
+
     }
 }
