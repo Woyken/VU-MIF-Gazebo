a9b71abf524512546e559fe140e1258bc9589845
monika.gasparaite <monika.gasparaite@gmail.com>
2018-05-29 21:08:07 +0300

    GZ-0379: Beans configuration added

--- src/main/java/lt/vu/mif/App.java
+++ src/main/java/lt/vu/mif/App.java
@@ -1,32 +1,23 @@
 package lt.vu.mif;
 
-import lt.vu.mif.generator.DataGenerator;
-import lt.vu.mif.generator.MockDataGenerator;
+import lt.vu.mif.generator.interfaces.IDataGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(scanBasePackages = {"lt"})
-@EnableJpaRepositories(basePackages = {"lt"})
-@EntityScan(basePackages = {"lt"})
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"lt"})
+@ImportResource("classpath:beans.xml")
 public class App {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(App.class);
 
-        DataGenerator dataGenerator = context.getBean(DataGenerator.class);
-        dataGenerator.initializeDatabase();
-
-        MockDataGenerator mockDataGenerator = context.getBean(MockDataGenerator.class);
-
-        mockDataGenerator.insertProducts();
-        mockDataGenerator.insertUsers();
-        mockDataGenerator.insertOrders();
+        IDataGenerator dataGenerator = (IDataGenerator) context.getBean("dataGenerator");
+        dataGenerator.generateData();
     }
 }
