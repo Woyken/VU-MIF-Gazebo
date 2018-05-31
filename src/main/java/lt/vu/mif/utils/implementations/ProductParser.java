e338830143cb67a5049ce00dc191f5b0149799e8
monika.gasparaite <monika.gasparaite@gmail.com>
2018-06-01 01:36:10 +0300

    Import fix

--- src/main/java/lt/vu/mif/utils/implementations/ProductParser.java
+++ src/main/java/lt/vu/mif/utils/implementations/ProductParser.java
@@ -2,9 +2,6 @@ package lt.vu.mif.utils.implementations;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.swing.text.html.parser.Entity;
-
 import lt.vu.mif.excel.ExcelProduct;
 import lt.vu.mif.excel.ImportResult;
 import lt.vu.mif.model.product.Category;
@@ -14,9 +11,7 @@ import lt.vu.mif.model.product.Image;
 import lt.vu.mif.model.product.Product;
 import lt.vu.mif.model.product.ProductAttributeValue;
 import lt.vu.mif.repository.repository.implementations.CategoryRepository;
-import lt.vu.mif.ui.view.AttributeView;
 import lt.vu.mif.utils.interfaces.IProductParser;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,6 +31,7 @@ public class ProductParser implements IProductParser {
             product.setTitle(excelProduct.getTitle());
             product.setDescription(excelProduct.getDescription());
             product.setPrice(excelProduct.getPrice());
+            product.setCategory(excelProduct.getCategory());
 
             for (byte[] bytes : excelProduct.getImagesBytes()) {
                 product.getImages().add(new Image(bytes));
@@ -48,31 +44,28 @@ public class ProductParser implements IProductParser {
     }
 
     private void parseAttributes(ExcelProduct product, Product entity) {
-        Category category = categoryRepository.get(product.getCategory().getId());
+        Category category = categoryRepository.get(entity.getCategory().getId());
+        while (category != null) {
+            for (CategoryAttribute categoryAttribute : category.getAttributes()) {
+                String valueForAttribute = product.getAttributes().get(categoryAttribute.getName());
 
-        if (category != null) {
-            while (category != null) {
-                for (CategoryAttribute categoryAttribute : category.getAttributes()) {
-                    String valueForAttribute = product.getAttributes().get(categoryAttribute.getName());
-
-                    CategoryAttributeValue value = null;
-                    for (CategoryAttributeValue val : categoryAttribute.getValues()) {
-                        if (val.getValue().equals(valueForAttribute)) {
-                            value = val;
-                            break;
-                        }
-                    }
-
-                    if (value != null) {
-                        ProductAttributeValue productAttributeValue = new ProductAttributeValue();
-                        productAttributeValue.setCategoryAttributeValue(value);
-                        productAttributeValue.setProduct(entity);
-                        entity.getAttributeValues().add(productAttributeValue);
+                CategoryAttributeValue value = null;
+                for (CategoryAttributeValue val : categoryAttribute.getValues()) {
+                    if (val.getValue().equals(valueForAttribute)) {
+                        value = val;
+                        break;
                     }
                 }
 
-                category = category.getParentCategory();
+                if (value != null) {
+                    ProductAttributeValue productAttributeValue = new ProductAttributeValue();
+                    productAttributeValue.setCategoryAttributeValue(value);
+                    productAttributeValue.setProduct(entity);
+                    entity.getAttributeValues().add(productAttributeValue);
+                }
             }
+
+            category = category.getParentCategory();
         }
     }
 }
--- src/tests/java/lt/vu/mif/Excel/ExcelReaderTest.java
+++ src/tests/java/lt/vu/mif/Excel/ExcelReaderTest.java
@@ -3,12 +3,9 @@ package lt.vu.mif.Excel;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.List;
-
 import lt.vu.mif.excel.ImportResult;
 import lt.vu.mif.excel.ProductExcelReader;
-import lt.vu.mif.model.product.Category;
-import lt.vu.mif.model.product.CategoryAttribute;
-import lt.vu.mif.model.product.CategoryAttributeValue;
+import lt.vu.mif.generator.interfaces.IDataGenerator;
 import lt.vu.mif.model.product.Product;
 import lt.vu.mif.repository.repository.interfaces.ICategoryRepository;
 import lt.vu.mif.repository.repository.interfaces.IProductRepository;
@@ -25,7 +22,7 @@ import org.springframework.util.ResourceUtils;
 @RunWith(SpringRunner.class)
 public class ExcelReaderTest {
 
-    private static final String FILE_NAME = "/tests_products.xlsx";
+    private static final String FILE_NAME = "/products.xlsx";
 
     @Autowired
     private ProductExcelReader productExcelReader;
@@ -35,96 +32,12 @@ public class ExcelReaderTest {
     private ICategoryRepository categoryRepository;
     @Autowired
     private IProductRepository productRepository;
+    @Autowired
+    private IDataGenerator dataGenerator;
 
     @Before
     public void init() {
-        Category electronics = new Category();
-        electronics.setName("electronics");
-
-        CategoryAttribute color = new CategoryAttribute();
-        color.setCategory(electronics);
-        color.setName("color");
-
-        CategoryAttributeValue black = new CategoryAttributeValue();
-        black.setCategoryAttribute(color);
-        black.setValue("black");
-
-        CategoryAttributeValue red = new CategoryAttributeValue();
-        red.setCategoryAttribute(color);
-        red.setValue("red");
-
-        CategoryAttributeValue white = new CategoryAttributeValue();
-        white.setCategoryAttribute(color);
-        white.setValue("white");
-
-        color.getValues().add(black);
-        color.getValues().add(white);
-
-        electronics.getAttributes().add(color);
-
-        Category phones = new Category();
-        phones.setName("phones");
-//
-//        CategoryAttribute size = new CategoryAttribute();
-//        size.setCategory(phones);
-//        color.setName("size");
-//
-//        CategoryAttributeValue fifteen = new CategoryAttributeValue();
-//        fifteen.setCategoryAttribute(size);
-//        fifteen.setValue("15");
-//
-        Category smartPhones = new Category();
-        smartPhones.setName("smart-phones");
-//
-//        CategoryAttribute manufacturerer = new CategoryAttribute();
-//        manufacturerer.setCategory(smartPhones);
-//        manufacturerer.setName("manufacturer");
-//
-//        CategoryAttribute bateryLife = new CategoryAttribute();
-//        bateryLife.setCategory(smartPhones);
-//        bateryLife.setName("batery life");
-//
-//        CategoryAttribute fifteen1 = new CategoryAttribute();
-//        fifteen1.setCategory(smartPhones);
-//        fifteen1.setName("15hrs");
-//
-//        CategoryAttributeValue something = new CategoryAttributeValue();
-//        something.setCategoryAttribute(manufacturerer);
-//        something.setValue("something");
-//
-//
-        smartPhones.setParentCategory(phones);
-        phones.setParentCategory(electronics);
-
-        Category other = new Category();
-        other.setName("other");
-
-        Category smartWatches = new Category();
-        smartWatches.setName("smart-watches");
-
-        smartWatches.setParentCategory(other);
-        other.setParentCategory(electronics);
-
-        Category houseAppliances = new Category();
-        houseAppliances.setName("house-appliances");
-
-        Category other2 = new Category();
-        other2.setName("other2");
-
-        other2.setParentCategory(houseAppliances);
-        houseAppliances.setParentCategory(electronics);
-
-        Category test = new Category();
-        test.setName("TEST");
-
-        categoryRepository.save(electronics);
-        categoryRepository.save(phones);
-        categoryRepository.save(smartPhones);
-        categoryRepository.save(other);
-        categoryRepository.save(smartWatches);
-        categoryRepository.save(houseAppliances);
-        categoryRepository.save(other2);
-        categoryRepository.save(test);
+        dataGenerator.generateData();
     }
 
     @Test
